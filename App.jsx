50513779aaed198df4d30717681bd9b8fc100e4c
Emilia Burza <emila.burza@gmail.com>
2024-09-06 11:52:15 +0200

    Add input components for bill and number of peoplr section

--- App.jsx
+++ App.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import PrecentageButtons from './components/PrecentageButtons'
+import FormInputComponent from './components/FormInputComponent'
 
 export default function App() {
 
     return (
-        <div id="page-wrapper">
+        <div id='page-wrapper'>
             <header>
-                <img id="logo" src="./images/logo.svg" alt="splitter logo" />
+                <img id='logo' src='./images/logo.svg' alt='splitter logo' />
             </header>
-            < PrecentageButtons />
+            <FormInputComponent 
+                name='bill'
+                label='Bill'/>
+            <PrecentageButtons />
+            <FormInputComponent 
+                name='people'
+                label='Number of People'/>
             <h1>App tip</h1>
         </div>
     )
